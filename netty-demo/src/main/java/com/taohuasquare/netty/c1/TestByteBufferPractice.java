1ffdc121157c27edb95713ca59c4474926232299
happy <18380463469@163.com>
2022-01-17 11:12:09 +0800

    ByteBuffer case

---
+++ netty-demo/src/main/java/com/taohuasquare/netty/c1/TestByteBufferPractice.java
@@ -0,0 +1,35 @@
+package com.taohuasquare.netty.c1;
+
+import java.nio.ByteBuffer;
+
+import static com.taohuasquare.netty.c1.ByteBufferUtil.debugAll;
+
+/**
+ * @author happy
+ * @since 2022/1/17
+ */
+public class TestByteBufferPractice {
+    public static void main(String[] args) {
+        ByteBuffer source = ByteBuffer.allocate(32);
+        source.put("Hello,world\nI'm zhangsan\nHo".getBytes());
+        split(source);
+        source.put("w are you?\nhaha!\n".getBytes());
+        split(source);
+    }
+
+    private static void split(ByteBuffer source) {
+        source.flip();
+        int limit = source.limit();
+        for (int i = 0; i < limit; i++) {
+            if (source.get(i) == '\n') {
+                ByteBuffer target = ByteBuffer.allocate(i + 1 - source.position());
+                // 0 ~ limit
+                source.limit(i + 1);
+                target.put(source); // 从source 读，向 target 写
+                debugAll(target);
+                source.limit(limit);
+            }
+        }
+        source.compact();
+    }
+}
---
+++ netty-demo/src/main/java/com/taohuasquare/netty/c1/TestScatteringReads.java
@@ -0,0 +1,34 @@
+package com.taohuasquare.netty.c1;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import static com.taohuasquare.netty.c1.ByteBufferUtil.debugAll;
+
+/**
+ * @author happy
+ * @since 2022/1/17
+ */
+@Slf4j
+public class TestScatteringReads {
+    public static void main(String[] args) {
+        try (FileChannel channel = new RandomAccessFile("work.txt", "r").getChannel()) {
+            ByteBuffer buffer1 = ByteBuffer.allocate(3);
+            ByteBuffer buffer2 = ByteBuffer.allocate(3);
+            ByteBuffer buffer3 = ByteBuffer.allocate(5);
+            channel.read(new ByteBuffer[]{buffer1, buffer2, buffer3});
+            buffer1.flip();
+            buffer2.flip();
+            buffer3.flip();
+            debugAll(buffer1);
+            debugAll(buffer2);
+            debugAll(buffer3);
+        } catch (IOException e) {
+            log.error("error:{}", e.getMessage());
+        }
+    }
+}
